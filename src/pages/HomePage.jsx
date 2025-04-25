c84bf6411b3842ebc8796d0fdf5b942f8ab806a2
Suganth-Dev <Suganth2501@gmail.com>
2025-04-25 13:30:49 +0530

    house of creatorsComponent name changed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Hero from '../components/home/Hero';
-import Features from '../components/home/Features';
+import Hero from '../Components/home/Hero';
+import Features from '../Components/home/Features';
 import Community from '../Components/home/Community';
-import Spaces from '../components/home/Spaces';
-import Testimonials from '../components/home/Testimonials';
-import Contact from '../components/home/Contact';
-import Cta from '../components/home/Cta';
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
+import Spaces from '../Components/home/Spaces';
+import Testimonials from '../Components/home/Testimonials';
+import Contact from '../Components/home/Contact';
+import Cta from '../Components/home/Cta';
+import Header from '../Components/layout/Header';
+import Footer from '../Components/layout/Footer';
 
 const HomePage = () => {
   return (
